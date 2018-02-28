2522009c181e9a15892ae7d3e2ad701957f10feb
Eduardo Riol Fernández <eriol@atsistemas.com>
2018-02-28 20:29:47 +0100

    Definimos el primer paso de la feature para abrir la web de DC Comics

---
+++ src/test/java/stepdefinitions/Steps.java
@@ -0,0 +1,23 @@
+package stepdefinitions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import cucumber.api.java.es.Dado;
+
+public class Steps {
+
+	public static WebDriver driver;
+	private final String url = "http://www.dccomics.com";
+
+	@Dado("^que estoy en la web de DC Comics$")
+	public void que_estoy_en_la_web_de_dc_comics() {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		driver = new ChromeDriver(options);
+		driver.get(url);
+		driver.manage().window().maximize();
+	}
+
+}
